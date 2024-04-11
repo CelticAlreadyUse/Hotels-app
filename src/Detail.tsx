5119f30cece6232b64ae3a19c71655f79dbbf4cd
CelticAlreadyUse <wahyusantosokanisius@gmail.com>
2024-04-12 01:55:17 +0700

    adding coursel and add data to style,im very sleepy so im not have make it done to much

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { firestore } from './libs/controller'
 import { NewHotelsTypes } from './types/hotels'
+import { Carousel } from 'flowbite-react'
 
 
 const Detail = ({}) => {
-  const [hotel,setHotels] = useState<NewHotelsTypes[]>([])
+  const [hotel,setHotels] = useState<NewHotelsTypes[] >([])
     const {id} = useParams()
     const getHotelsId = async()=>{
         const hotelsCollection = collection(firestore,'hotels')
@@ -19,8 +20,31 @@ const Detail = ({}) => {
       getHotelsId()
     },[])
     return (
-    <div className='mt-[80px] ml'>Detail {id}
-    <img src={hotel[0]?.img_url?hotel[0].img_url[0]:''}></img>
+    <div className='mt-[80px]'>
+      <Carousel>
+    {hotel.map((hotel) => (
+      Array.isArray(hotel.img_url) ? 
+        hotel.img_url.map((url) =>
+        <>
+        <img className='w-full' width={2500} height={2500} key={url} src={url}></img>
+        </>
+        )  :
+        <img src={hotel.img_url}></img>
+    ))}
+    </Carousel>
+    {hotel.map((hotel)=>(
+      <>
+              <p>{hotel.name}</p>
+        <p>{hotel.pernight}</p>
+        <p>{hotel.location}</p>
+        <p>{hotel.description}</p>
+        <p>{hotel.feature}</p>
+        <p>{hotel.region}</p>
+        <p>{hotel.website}</p>
+        <p>{hotel.riview}</p>
+        </>
+    ))}
+
     </div>
   )
 }
